8251a018176abc6fac6db40d996034a63574f12b
paulsejo <kamprimus@gmail.com>
2023-02-22 11:24:17 +0100

    dockerisiert und Ordnerstruktur aktualisiert

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./dogsRoutes.js";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use("/dogs", router);
+
+app.listen(port, () => {
+  console.log(`Server is running on: http://localhost:${port}`);
+});
+
+mongoose.connect("mongodb://localhost:27017/dogs").then(() => {
+  console.log("Connected to database");
+});
